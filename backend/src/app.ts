9c7a6bae6834491f9ea4f8f6ad8cb5ef3dfce8a4
Saumya Srivastava <samdevpro99@gmail.com>
2026-02-06 20:44:43 +0530

    rollback: last commit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,13 +5,7 @@ import errorMiddleware from './middlewares/error.middleware';
 
 const app: express.Application = express();
 
-app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://dynamic-portfolio-bay.vercel.app"
-  ]
-}));
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/portfolio', portfolioRoutes);
