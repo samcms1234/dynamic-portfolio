0804b438d8bcd1f3e33db857d6adceba34acef57
Saumya Srivastava <samdevpro99@gmail.com>
2026-02-06 20:38:50 +0530

    fix: allowed origin for vercel

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,7 +5,13 @@ import errorMiddleware from './middlewares/error.middleware';
 
 const app: express.Application = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://dynamic-portfolio-bay.vercel.app"
+  ]
+}));
+
 app.use(express.json());
 
 app.use('/api/portfolio', portfolioRoutes);
